156602a2df998da25e14c215315a806679b05a97
Jakub Kubacki <kubacki.jk@gmail.com>
2014-11-15 11:30:07 +0100

    Add name validation to gift

--- spec/models/gift_spec.rb
+++ spec/models/gift_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 RSpec.describe Gift, :type => :model do
   it { is_expected.to belong_to(:list) }
   it { is_expected.to belong_to(:buyer) }
+  it { is_expected.to validate_presence_of(:name) }
 end
