035c75a85c09ca114cb84b53a75118c51f5846cb
Jakub Kubacki <kubacki.jk@gmail.com>
2014-11-15 12:01:24 +0100

    Simplify routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,10 +10,7 @@ Rails.application.routes.draw do
   root to: 'visitors#index'
   devise_for :users
 
-  resources :users do
-    resources :lists do
-      resources :gifts
-    end
-  end
+  resources :users
+  resources :lists
 
 end
