56450913144dca60519051cb88e616cc6285ccc7
Malgorzata Ksionek <meksionek@gmail.com>
2014-11-15 11:41:17 +0100

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,11 @@
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
 # Environment variables (ENV['...']) can be set in the file .env file.
+
+User.delete_all
+
+
+user1 = User.create! :name => 'Kalinda', :email => 'kalinda@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret'
+user2 = User.create! :name => 'Anna', :email => 'anna@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret'
+user3 = User.create! :name => 'Kate', :email => 'kate@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret'
+user4 = User.create! :name => 'Beth', :email => 'beth@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret'
