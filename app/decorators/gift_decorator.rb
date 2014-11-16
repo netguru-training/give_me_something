215f62731353e7295053fcf31ab1cc9e9597a3b8
Jakub Kubacki <kubacki.jk@gmail.com>
2014-11-16 13:27:14 +0100

    Search for gifts icons

--- app/decorators/gift_decorator.rb
+++ app/decorators/gift_decorator.rb
@@ -8,5 +8,25 @@ class GiftDecorator < Draper::Decorator
       return "warning"
     end
   end
+
+  def allegro_url
+    "http://allegro.pl/listing/listing.php?string=#{ name_escaped }"
+  end
+
+  def ebay_url
+    "http://www.ebay.com/sch/i.html?&_nkw=#{ name_escaped }"
+  end
+
+  def amazon_url
+    "http://www.amazon.com/s/ref=nb_sb_noss?&field-keywords=#{ name_escaped }"
+  end
+
+  def google_url
+    "https://www.google.pl/#q=#{ name_escaped }+sklep"
+  end
+
+  def name_escaped
+    CGI.escape name.downcase
+  end
   
 end
