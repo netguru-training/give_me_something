006fa04be623b55572383e532f3ee3b17780dfa2
Malgorzata Ksionek <meksionek@gmail.com>
2014-11-16 14:48:46 +0100

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,24 +7,37 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 # Environment variables (ENV['...']) can be set in the file .env file.
 
-User.delete_all
-List.delete_all
-Gift.delete_all
 
-User.create!(:name => "Testuser", :email => "testuser@test.com", :password => "password", :password_confirmation => "password")
-User.create!(:name => "Testuser2", :email => "testuser2@test.com", :password => "password", :password_confirmation => "password")
+User.create(:name => "Testuser", 
+  :email => "testuser@test.com", 
+  :password => "password", 
+  :password_confirmation => "password")
+User.create(:name => "Testuser2", 
+  :email => "testuser2@test.com", 
+  :password => "password", 
+  :password_confirmation => "password")
 
 
-5.times do
+10.times do
   pass = "password"
-  User.create! :name => Faker::Name.name, :email => Faker::Internet.email, :password => pass, :password_confirmation => pass
+  User.create! :name => Faker::Name.name, 
+    :email => Faker::Internet.email, 
+    :password => pass, 
+    :password_confirmation => pass
 end
 
 User.all.each do |user|
- 3.times {user.lists.create(:name => Faker::Lorem.word)}
+ 3.times {user.lists.create(:name => Faker::Company.catch_phrase)}
 end
 
 List.all.each do |list|
-  3.times {list.gifts.create(:name => Faker::Lorem.word)}
+  Random.rand(5..15).times do
+    buyer = if [true, false].sample
+      User.all.sample
+    else
+      nil
+    end
+    list.gifts.create(:name => Faker::Company.catch_phrase, :description => Faker::Lorem.paragraph, :buyer => buyer)
+  end
 end
 
